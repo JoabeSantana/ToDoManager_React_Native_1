d49c34baef2a8bd9259796cf2bb4fa1a794ce676
Joabe Santana Correia <joabesantana.c@gmail.com>
2020-04-26 14:08:36 -0300

    Restruturação do projeto

---
+++ src/screens/App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.bigBlue}>Big Blue</Text>
+      <Text style={styles.smallRed}>Small Red</Text>
+    </View>);
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', alignItems: 'center'
+  }, bigBlue: {
+    color: 'blue',
+    fontSize: 50
+  },
+  smallRed: {
+    color: 'red',
+    fontSize: 20
+  }
+});
+
+export default App;
